ed271a0f69699ef734d7bc419e150594ce7737ed
GustavoRamos112 <110934504+GustavoRamos112@users.noreply.github.com>
2024-08-22 23:01:42 -0600

    Función para exportar los datos.

---
+++ Exportar_datos.hpp
@@ -0,0 +1,22 @@
+void Exportar_datos(double **A, int renglones_A, int columnas_A, string operacion) {
+    ofstream resultado;
+    resultado.open("../Resultado.dat", fstream::out);
+    /* resultado << "import numpy as np\n\n";
+    resultado << "class matriz:\n";
+    resultado << "\tC = np.array(["; */
+    resultado << "renglones = " << renglones_A << "\n"
+              << "columnas = " << columnas_A << "\n"
+              << "operacion = " << operacion << "\n";
+    
+    for (int i = 0; i < renglones_A; i++) {
+        resultado << "{";
+        for (int j = 0; j < columnas_A; j++) {
+            resultado << A[i][j];
+            (j == columnas_A-1 ) ? resultado << "": resultado << ",";
+        }
+        resultado << "}";
+        (i == renglones_A-1 ) ? resultado << "": resultado << "\n";
+    }
+
+    resultado.close();
+}
